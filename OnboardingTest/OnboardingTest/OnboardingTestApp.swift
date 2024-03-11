c299cc80511c0d41470c23ee68f8073ff75c0887
JIN-JJS <joonsu2005@naver.com>
2024-03-11 17:23:31 +0900

    [Add] Create Project

---
+++ OnboardingTest/OnboardingTest/OnboardingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingTestApp.swift
+//  OnboardingTest
+//
+//  Created by 전준수 on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
